3480cf75a5759399701c9d843d182dd8b211f528
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-07-02 03:37:36 +0700

    updated model user for parse photo

--- lib/core/model/userModel.dart
+++ lib/core/model/userModel.dart
@@ -2,22 +2,32 @@ class User {
   final int id;
   final String username;
   final String role;
+  final String? photo;
 
   User({
     required this.id,
     required this.username,
     required this.role,
+    this.photo,
   });
 
-  factory User.fromJson(Map<String, dynamic> json) => User(
-        id: json["id"],
-        username: json["username"],
-        role: json["role"],
-      );
+  factory User.fromJson(Map<String, dynamic> json) {
+    String? photo = json['photo'];
+    if (photo != null && photo.isNotEmpty && !photo.startsWith('http')) {
+      photo = 'http://192.168.1.21:3001/uploads/$photo';
+    }
+    return User(
+      id: json['id'],
+      username: json['username'],
+      role: json['role'],
+      photo: photo,
+    );
+  }
 
   Map<String, dynamic> toJson() => {
         "id": id,
         "username": username,
         "role": role,
+        "photo": photo, // Tambahkan field photo jika ada
       };
 }
