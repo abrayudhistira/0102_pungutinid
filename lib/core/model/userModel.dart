034d759430dba8fb81fca30eea9de6ea37810ba1
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-07-03 00:33:18 +0700

    updated user model

--- lib/core/model/userModel.dart
+++ lib/core/model/userModel.dart
@@ -1,33 +1,49 @@
 class User {
   final int id;
-  final String username;
-  final String role;
-  final String? photo;
+  final String? username;
+  final String? role;
+  final String photo;
+  final String? fullname;
+  final String? email;
+  final String? address;
+  final String? phone;
 
   User({
     required this.id,
-    required this.username,
-    required this.role,
-    this.photo,
+    this.username,
+    this.role,
+    required this.photo,
+    this.fullname,
+    this.email,
+     this.address,
+    this.phone,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
     String? photo = json['photo'];
     if (photo != null && photo.isNotEmpty && !photo.startsWith('http')) {
-      photo = 'http://192.168.1.21:3001/uploads/$photo';
+      photo = 'http://10.0.2.2:3001/uploads/$photo';
     }
     return User(
-      id: json['id'],
+      id: json['user_id'], // sesuaikan dengan field backend
       username: json['username'],
       role: json['role'],
-      photo: photo,
+      photo: photo ?? '',
+      fullname: json['fullname'],
+      email: json['email'],
+      address: json['address'],
+      phone: json['phone'],
     );
   }
 
   Map<String, dynamic> toJson() => {
-        "id": id,
+        "user_id": id,
         "username": username,
         "role": role,
-        "photo": photo, // Tambahkan field photo jika ada
+        "photo": photo,
+        "fullname": fullname,
+        "email": email,
+        "address": address,
+        "phone": phone,
       };
 }
