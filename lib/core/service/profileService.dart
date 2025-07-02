826fe10c1117faa71fcb21ed498bd7a3197710bf
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-07-03 00:33:50 +0700

    update profile service

--- lib/core/service/profileService.dart
+++ lib/core/service/profileService.dart
@@ -1,18 +1,29 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
-import 'package:pungutinid/core/service/authService.dart'; // pastikan import ini
+import 'package:pungutinid/core/service/authService.dart';
 
 class ProfileService {
   Future<void> updateProfile({
+    required int userId,
     required String username,
     String? password,
     File? photo,
+    required String fullname,
+    required String email,
+    required String address,
+    required String phone,
   }) async {
-    final uri = Uri.parse('http://192.168.1.21:3001/profile/edit');
+    final uri = Uri.parse('http://10.0.2.2:3001/profile/edit');
     var request = http.MultipartRequest('POST', uri);
 
+    request.fields['user_id'] = userId.toString(); // <-- Tambahkan ini
     request.fields['username'] = username;
+    request.fields['fullname'] = fullname;
+    request.fields['email'] = email;
+    request.fields['address'] = address;
+    request.fields['phone'] = phone;
+
     if (password != null && password.isNotEmpty) {
       request.fields['password'] = password;
     }
@@ -26,6 +37,12 @@ class ProfileService {
       );
     }
 
+    // Debug print data yang dikirim
+    print('[DEBUG] UpdateProfile fields: ${request.fields}');
+    if (photo != null) {
+      print('[DEBUG] UpdateProfile photo: ${photo.path}');
+    }
+
     // Tambahkan token ke header
     final token = await AuthService().accessToken;
     if (token != null) {
@@ -34,6 +51,9 @@ class ProfileService {
 
     final response = await request.send();
     final respStr = await response.stream.bytesToString();
+
+    print('[DEBUG] UpdateProfile response: ${response.statusCode} - $respStr');
+
     if (response.statusCode != 200) {
       print('Update profile gagal: ${response.statusCode} - $respStr');
       throw Exception('Gagal update profile');
